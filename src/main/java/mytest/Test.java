5b3322a3cff0d988cfd535c422c215126e35c2b9
pc <pc@jtwm>
2020-02-12 18:31:03 +0800

    dd

--- src/main/java/mytest/Test.java
+++ src/main/java/mytest/Test.java
@@ -3,7 +3,9 @@ package mytest;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub  
+		System.out.print("aa");
+		System.out.print("bb");
 	}
 
 }
